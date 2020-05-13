fa8383caa1a4c8ea05ab27d257cc79c689bc3447
yakamon <p.m.byzance0817@gmail.com>
2020-05-14 01:38:10 +0900

    2020-05-14 01:38:10

--- conf/util.sh
+++ conf/util.sh
@@ -1,11 +1,3 @@
-function bar() {
-	local REP="$1"
-	local N="$2"
-	local MESSAGE="$3"
-	printf "%${N}s\n" | sed "s/ /${REP}/g"
-	[[ "${MESSAGE}" != "" ]] && message "${MESSAGE}"
-}
-
 function message() {
 	local MESSAGE="$1"
 	local COLOR="$2"
