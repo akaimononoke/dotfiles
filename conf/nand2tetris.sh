f26d4c2124a65d98d416d306f851e75f0cf4c180
yakamon <p.m.byzance0817@gmail.com>
2020-05-27 02:02:37 +0900

    2020-05-27 02:02:37

--- conf/nand2tetris.sh
+++ conf/nand2tetris.sh
@@ -1,7 +1,7 @@
 NAND2TETRIS="/Users/yakamon/go/src/github.com/yakamon/books/TheElementOfComputingSystem/nand2tetris/tools"
 
-alias nand2tetris-HardwareSimulator='${NAND2TETRIS}/HardwareSimulator.sh'
-alias nand2tetris-CPUEmulator='${NAND2TETRIS}/CPUEmulator.sh'
-alias nand2tetris-Assembler='${NAND2TETRIS}/Assembler.sh'
-alias nand2tetris-VMEmulator='${NAND2TETRIS}/VMEmulator.sh'
-alias nand2tetris-JackCompiler='${NAND2TETRIS}/JackCompiler.sh'
+alias HardwareSimulator='${NAND2TETRIS}/HardwareSimulator.sh'
+alias CPUEmulator='${NAND2TETRIS}/CPUEmulator.sh'
+alias Assembler='${NAND2TETRIS}/Assembler.sh'
+alias VMEmulator='${NAND2TETRIS}/VMEmulator.sh'
+alias JackCompiler='${NAND2TETRIS}/JackCompiler.sh'
