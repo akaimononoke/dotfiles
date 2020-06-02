e0b216e8f872d02c7b4edcd8ac5bf2e8ad9ef746
yakamon <p.m.byzance0817@gmail.com>
2020-06-03 00:54:49 +0900

    2020-06-03 00:54:49

--- conf/nand2tetris.sh
+++ conf/nand2tetris.sh
@@ -1,4 +1,4 @@
-NAND2TETRIS="/Users/yakamon/go/src/github.com/yakamon/books/TheElementOfComputingSystem/nand2tetris/tools"
+NAND2TETRIS="/Users/yakamon/go/src/github.com/yakamon/books/TheElementsOfComputingSystem/nand2tetris/tools"
 
 alias HardwareSimulator='${NAND2TETRIS}/HardwareSimulator.sh'
 alias CPUEmulator='${NAND2TETRIS}/CPUEmulator.sh'
