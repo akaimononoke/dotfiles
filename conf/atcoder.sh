3e54e8d5a689a0c740ad038780cf5cfa90aa430e
yakamon <p.m.byzance0817@gmail.com>
2020-07-06 21:40:40 +0900

    2020-07-06 21:40:40

--- conf/atcoder.sh
+++ conf/atcoder.sh
@@ -1,7 +1,6 @@
 function atcodermkdir() {
     CONTEST_NAME="$1"
     PROBLEM_CODE="$2"
-    INPUT_NUM="$3"
 
     if [ $# -lt 1 ]; then
         printf "usage: atcoder-mkdir <contest_name> <problem_code> <number_of_input_files>\n"
